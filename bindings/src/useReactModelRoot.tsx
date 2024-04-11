505f51e798a8f8a306b9ee8517ff646a5e72b011
afonsocrg <afonso.crg@gmail.com>
2024-04-11 11:30:58 +0100

    update useReactModel to support nested models

--- bindings/src/useReactModelRoot.tsx
+++ bindings/src/useReactModelRoot.tsx
@@ -2,27 +2,40 @@ import { useState } from "react";
 import { useModelRoot, useSubscribe, usePublish } from "..";
 import { ReactModel } from "./ReactModel";
 
-export function useReactModelRoot<T extends ReactModel>(): T {
-    const model = useModelRoot() as T;
-  
-    const [modelState, setModelState] = useState({...model});
-    
-    useSubscribe(model.id, "react-updated", () => {
-      setModelState({...model});
-    });
-    
-    const methods: Partial<T> = {};
-    model.__reactEvents.forEach(({ scope, event }) => {
+function getModelObject<T extends ReactModel>(model: T): T {
+  const methods: Partial<T> = {};
+  const excludeMethods = new Set(["view-join", "view-exit"]);
+  model.__reactEvents
+    .filter((e) => !excludeMethods.has(e.event))
+    .forEach(({ scope, event }) => {
       // @ts-ignore
       methods[event] = usePublish((data) => [scope, event, data]);
     });
-    
-    const properties: Partial<T> = {};
-    for(const p in modelState) {
-      if(p !== '__reactEvents') {
-        properties[p] = modelState[p]
+
+  const properties: Partial<T> = {};
+  const excludeProperties = new Set(["__reactEvents"]);
+  for (const p in model) {
+    if (!excludeProperties.has(p)) {
+      const prop = model[p];
+      if (prop instanceof ReactModel) {
+        properties[p] = getModelObject(prop);
+      } else {
+        properties[p] = prop;
       }
     }
-  
-    return { ...properties, ...methods } as T;
   }
+
+  return { ...properties, ...methods } as T;
+}
+
+export function useReactModelRoot<T extends ReactModel>(): T {
+  const model = useModelRoot() as T;
+
+  const [modelState, setModelState] = useState({ ...model });
+
+  useSubscribe(model.id, "react-updated", () => {
+    setModelState({ ...model });
+  });
+
+  return getModelObject(modelState);
+}

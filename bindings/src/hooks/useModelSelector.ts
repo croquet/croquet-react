123d2d45f250f904dc81fbbc15d9d622661929d4
afonsocrg <afonso.crg@gmail.com>
2024-08-19 21:01:31 +0100

    useModelSelector: use hash to determine object changes

--- bindings/src/hooks/useModelSelector.ts
+++ bindings/src/hooks/useModelSelector.ts
@@ -1,10 +1,35 @@
 import { ReactModel } from '../ReactModel'
-import { useEffect, useState } from 'react'
+import { useEffect, useState, useCallback } from 'react'
 import { useCroquetContext } from './useCroquetContext'
+import hash_fn, { NotUndefined } from 'object-hash'
 
-export function useModelSelector<T extends ReactModel, V>(id: string, fn: (m: T | null) => V): V {
+interface UseModelSelectorOptions {
+  hash?: boolean
+}
+type ModelState<V> = {
+  value: V
+  hash: string | V
+}
+
+export function useModelSelector<T extends ReactModel, V>(
+  id: string,
+  selector: (m: T | null) => V,
+  { hash = false }: UseModelSelectorOptions = {}
+): V {
   const { session, view, model } = useCroquetContext()
-  const [modelState, setModelState] = useState(fn(model as T))
+
+  // The selector function may return a pointer to an object/array/etc,
+  // Storing the returned value's hash allows to determine if it has changed.
+  // The hash must be computed when the value is set, since
+  // doing it at compare time would result in the same output.
+  const hashObject = useCallback((obj: V): string | V => {
+    return hash ? hash_fn(obj as NotUndefined) : obj
+  }, [hash])
+
+  const [modelState, setModelState] = useState<ModelState<V>>(() => {
+    const value = selector(model as T)
+    return { value, hash: hashObject(value) }
+  })
 
   useEffect(() => {
     // console.log(`[${id}] Subscribing to react-updated`)
@@ -12,10 +37,12 @@ export function useModelSelector<T extends ReactModel, V>(id: string, fn: (m: T 
       const handler = () => {
         // console.log('@croquet/react: react-updated')
         setModelState((prev) => {
-          const newValue = fn(model as T)
-          if(prev !== newValue) {
+          const newValue = selector(model as T)
+          const newHash = hashObject(newValue)
+          if (prev.hash !== newHash) {
+            console.log({ id, prevHash: prev.hash, newHash, equals: prev.hash === newHash })
             // console.log({id, prev, newValue, equals: prev === newValue})
-            return newValue
+            return { value: newValue, hash: newHash }
           } else {
             // console.log(`[${id}] @croquet/react: react-updated`)
           }
@@ -37,5 +64,5 @@ export function useModelSelector<T extends ReactModel, V>(id: string, fn: (m: T 
     }
   }, [session, view, model])
 
-  return modelState
+  return modelState.value
 }

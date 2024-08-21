76ffb9c24e5cda101b2f8526cd5c5236a52de5c5
afonsocrg <afonso.crg@gmail.com>
2024-08-21 23:40:54 +0100

    useConnectedViews does not need a generic type

--- bindings/src/hooks/useConnectedViews.ts
+++ bindings/src/hooks/useConnectedViews.ts
@@ -15,10 +15,10 @@ function viewsSelector<T extends ReactModel>(rootModel: T | null): ConnectedView
     viewCount: views.size,
   }
 }
-export function useConnectedViews<T extends ReactModel>(): ConnectedViews {
+export function useConnectedViews(): ConnectedViews {
   const context = useCroquetContext()
   const { session, view } = context
-  const model = context.model as T
+  const model = context.model
 
   const [views, setViews] = useState(viewsSelector(model))
 
